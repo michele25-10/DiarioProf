bc060e7d5d4c16491985e1efdc185ac4f25374e2
michele25-10 <michele.gabri01@gmail.com>
2023-04-05 12:51:47 +0200

    Creata API getArchieveCorsi.php

--- backend/MODEL/corso.php
+++ backend/MODEL/corso.php
@@ -13,4 +13,14 @@ class Corso
         VALUES ('" . $tipologia . "', '" . $id_quadrimestre . "', '" . $id_docente . "', '" . $id_tutor . "', '" . $materia . "', '" . $data_inizio . "', '" . $data_fine . "', '" . $nome_corso . "', '" . $sede . "');";
         return $sql;
     }
+
+    function getArchiveCorso()
+    {
+        $sql = "SELECT c.id, c.tipologia, concat(q.data_inizo, '-',q.data_fine) as 'id_quadrimestre', concat(d.nome, d.cognome) as 'id_docente', concat(d2.nome, d2.cognome) as 'id_tutor', c.materia, c.data_inizio, c.data_fine, c.nome_corso, c.sede
+        FROM diario.corso c
+        INNER JOIN diario.quadrimestre q ON q.id = c.id_quadrimestre
+        INNER JOIN diario.docente d ON d.CF = c.id_docente
+        INNER JOIN diario.docente d2 ON d2.CF = c.id_tutor; ";
+        return $sql;
+    }
 }
