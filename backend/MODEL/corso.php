78533484c12c6ea1fa126e5efd9d9f087a1479b3
michele25-10 <michele.gabri01@gmail.com>
2023-04-05 12:20:59 +0200

    Creata API addCorso.php
    una insert into per inserire un corso all'interno del db.
    API di tipo POST

--- backend/MODEL/corso.php
+++ backend/MODEL/corso.php
@@ -6,5 +6,11 @@ class Corso
     {
         $this->conn = $db;
     }
+
+    function addCorso($tipologia, $id_quadrimestre, $id_docente, $id_tutor, $materia, $data_inizio, $data_fine, $nome_corso, $sede)
+    {
+        $sql = "INSERT INTO diario.corso(tipologia, id_quadrimestre, id_docente, id_tutor, materia, data_inizio, data_fine, nome_corso, sede)
+        VALUES ('" . $tipologia . "', '" . $id_quadrimestre . "', '" . $id_docente . "', '" . $id_tutor . "', '" . $materia . "', '" . $data_inizio . "', '" . $data_fine . "', '" . $nome_corso . "', '" . $sede . "');";
+        return $sql;
+    }
 }
-?>
