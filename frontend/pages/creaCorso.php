51dd345f132fdd4250ff218d2ba8d5076ea0d7dd
Marco-Malanchin <72445677+Marco-Malanchin@users.noreply.github.com>
2023-04-05 12:40:31 +0200

    pagina iniziale creazione corso

---
+++ frontend/pages/creaCorso.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Diario | CreaCorso</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <link rel="stylesheet" href="assets/style.css">
+        <link rel="icon" type="image/x-icon" href="../assets/img/logo.png">
+    </head>
+
+    <body>
+    <?php require_once(__DIR__.'\navbar.php'); ?>
+
+  <div class="container">
+    <div class="row mt-5">
+      <h2>Inserisci dati calciatore:</h2>
+    </div>
+    <div class="row mt-5">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col"></th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row"></th>
+            <form method="post">
+              <td>
+                <input class="form-control" type="" id="name" placeholder="Nome Giocatore" name="name"
+                  maxlength="50" required>
+              </td>
+              <td>
+                <input class="form-control" type="" id="surname" placeholder="Cognome Giocatore" name="surname"
+                  maxlength="50" required>
+              </td>
+              <td>
+                <input class="form-control" type="" id="role" placeholder="Ruolo" name="role"
+                  maxlength="50" required>
+              </td>
+              <td>
+                <button type="submit" class="btn btn-success" name="legha">Conferma</button>
+</td>
+        </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    </body>
+</html>
