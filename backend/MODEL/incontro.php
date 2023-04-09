17f7d699bb30f601513d878360cba130dd7742d5
michele25-10 <michele.gabri01@gmail.com>
2023-04-09 16:31:33 +0200

    API getArchieveIncontri
    Archivio incontri in ordine decrescente rispetto alle date.

--- backend/MODEL/incontro.php
+++ backend/MODEL/incontro.php
@@ -12,4 +12,12 @@ class Incontro
         VALUES ('" . $id_corso . "', '" . $data_inizio . "', '" . $note . "'); ";
         return $sql;
     }
+    function getArchieveIncontri()
+    {
+        $sql = "SELECT i.id, c.nome_corso as 'id_corso', i.data_inizio, i.note 
+        FROM diario.incontro i
+        INNER JOIN diario.corso c ON c.id = i.id_corso
+        order by i.data_inizio desc;";
+        return $sql;
+    }
 }
