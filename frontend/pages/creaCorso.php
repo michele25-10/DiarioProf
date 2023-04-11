b7ffe2408ec8fc13f811c49f548dfee8d5522c68
michele25-10 <michele.gabri01@gmail.com>
2023-04-11 15:16:10 +0200

    Sistemato il codice di Jquery pagina creaCorso.php

--- frontend/pages/creaCorso.php
+++ frontend/pages/creaCorso.php
@@ -27,63 +27,11 @@ session_start();
   $list_al = getArchieveAlunni();
   ?>
 
-  <?php
-  include_once dirname(__FILE__) . '\..\function\corsi.php';
-  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $count = countCorsoByType($_POST['tipologia']);
-    if ($count != -1) {
-      $count = $count + 1;
-      if ($count < 10) {
-        $nome_corso = "Corso_" . $_POST['tipologia'] . "_00" . $count;      //creo il nome del corso unendo la tipologia con il numero del corso
-      } elseif ($count > 9 && $count < 100) {
-        $nome_corso = "Corso_" . $_POST['tipologia'] . "_0" . $count;      //creo il nome del corso unendo la tipologia con il numero del corso
-      } elseif ($count > 99) {
-        $nome_corso = "Corso_" . $_POST['tipologia'] . "_" . $count;      //creo il nome del corso unendo la tipologia con il numero del corso
-      }
-
-      if ($_POST['tipologia'] == 'A' || $_POST['tipologia'] == 'B') {
-        $data = array(
-          "tipologia" => $_POST['tipologia'],
-          "id_quadrimestre" => $_POST['id_quadrimestre'],
-          "id_docente" => $_POST['id_docente'],
-          "id_tutor" => "-1",
-          "materia" => $_POST['materia'],
-          "data_inizio" => $_POST['data_inizio'],
-          "data_fine" => $_POST['data_fine'],
-          "nome_corso" => $nome_corso,
-          "sede" => $_POST['sede'],
-        );
-      }
-      if ($_POST['tipologia'] == 'C') {
-        $data = array(
-          "tipologia" => $_POST['tipologia'],
-          "id_quadrimestre" => $_POST['id_quadrimestre'],
-          "id_docente" => $_POST['id_docente'],
-          "id_tutor" => $_POST['id_tutor'],
-          "materia" => $_POST['materia'],
-          "data_inizio" => $_POST['data_inizio'],
-          "data_fine" => $_POST['data_fine'],
-          "nome_corso" => $nome_corso,
-          "sede" => $_POST['sede'],
-        );
-      }
-      $res = addCorso($data);
-      if ($res == 1) {
-      }
-      if ($res == -1) {
-        echo ('<p class="text-danger"><b>Errore!</b></p>');
-      }
-    } else {
-      echo ('<p class="text-danger"><b>Errore!</b></p>');
-    }
-  }
-  ?>
-
   <div class="container mt-5">
     <div class="progress mb-5">
       <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>
     </div>
-    <form id="regiration_form" novalidate action="action.php" method="post">
+    <form id="regiration_form" method="post">
       <fieldset>
         <h2>Corso: </h2>
         <div class="row mt-1">
@@ -180,6 +128,7 @@ session_start();
               <?php foreach ($list_al as $row) : ?>
                 <option value="<?php echo $row['CF'] ?>"><?php echo ($row['nome'] . " " . $row['cognome']) ?></option>
               <?php endforeach ?>
+            </select>
           </div>
           <div id="alunni-typeB">
 
@@ -206,18 +155,21 @@ session_start();
               $("#html").remove();
               $('#form-typeA').html('<div id="typeA"><label class="form-label">Quinto incontro</label><input type="datetime-local" class="form-control" name="incontro5" id="fName"></div>');
               $("#form-alunni-typeB").remove();
+              $("#form-alunni-typeC").remove();
               break;
 
             case "B":
               $("#html").remove();
               $("#typeA").remove();
-              $('#alunni-typeB').html('<div id="form-alunni-typeB"><div class="col-md-6 mb-3"><label class="form-label">Alunno:</label><select class="form-select" aria-label="Default select example" name="alunno2" required><option selected disabled>Alunni:</option><?php foreach ($list_al as $row) : ?><option value="<?php echo $row['CF'] ?>"><?php echo ($row['nome'] . " " . $row['cognome']) ?></option><?php endforeach ?></div><div class="col-md-6 mb-3"><label class="form-label">Alunno:</label><select class="form-select" aria-label="Default select example" name="alunno3" required><option selected disabled>Alunni:</option><?php foreach ($list_al as $row) : ?><option value="<?php echo $row['CF'] ?>"><?php echo ($row['nome'] . " " . $row['cognome']) ?></option><?php endforeach ?></div><div class="col-md-6 mb-3"><label class="form-label">Alunno:</label><select class="form-select" aria-label="Default select example" name="alunno4" required><option selected disabled>Alunni:</option><?php foreach ($list_al as $row) : ?><option value="<?php echo $row['CF'] ?>"><?php echo ($row['nome'] . " " . $row['cognome']) ?></option><?php endforeach ?></div><div class="col-md-6 mb-3"><label class="form-label">Alunno:</label><select class="form-select" aria-label="Default select example" name="alunno5" required><option selected disabled>Alunni:</option><?php foreach ($list_al as $row) : ?><option value="<?php echo $row['CF'] ?>"><?php echo ($row['nome'] . " " . $row['cognome']) ?></option><?php endforeach ?></div><div class="col-md-6 mb-3"><label class="form-label">Alunno:</label><select class="form-select" aria-label="Default select example" name="alunno6" required><option selected disabled>Alunni:</option><?php foreach ($list_al as $row) : ?><option value="<?php echo $row['CF'] ?>"><?php echo ($row['nome'] . " " . $row['cognome']) ?></option><?php endforeach ?></div></div>');
+              $('#alunni-typeB').html('<div id="form-alunni-typeB"><div class="col-md-6 mb-3"><label class="form-label">Alunno:</label><select class="form-select" aria-label="Default select example" name="alunno2" required><option selected disabled>Alunni:</option><?php foreach ($list_al as $row) : ?><option value="<?php echo $row['CF'] ?>"><?php echo ($row['nome'] . " " . $row['cognome']) ?></option><?php endforeach ?></select></div><div class="col-md-6 mb-3"><label class="form-label">Alunno:</label><select class="form-select" aria-label="Default select example" name="alunno3" required><option selected disabled>Alunni:</option><?php foreach ($list_al as $row) : ?><option value="<?php echo $row['CF'] ?>"><?php echo ($row['nome'] . " " . $row['cognome']) ?></option><?php endforeach ?></select></div><div class="col-md-6 mb-3"><label class="form-label">Alunno:</label><select class="form-select" aria-label="Default select example" name="alunno4" required><option selected disabled>Alunni:</option><?php foreach ($list_al as $row) : ?><option value="<?php echo $row['CF'] ?>"><?php echo ($row['nome'] . " " . $row['cognome']) ?></option><?php endforeach ?></select></div><div class="col-md-6 mb-3"><label class="form-label">Alunno:</label><select class="form-select" aria-label="Default select example" name="alunno5" required><option selected disabled>Alunni:</option><?php foreach ($list_al as $row) : ?><option value="<?php echo $row['CF'] ?>"><?php echo ($row['nome'] . " " . $row['cognome']) ?></option><?php endforeach ?></select></div><div class="col-md-6 mb-3"><label class="form-label">Alunno:</label><select class="form-select" aria-label="Default select example" name="alunno6" required><option selected disabled>Alunni:</option><?php foreach ($list_al as $row) : ?><option value="<?php echo $row['CF'] ?>"><?php echo ($row['nome'] . " " . $row['cognome']) ?></option><?php endforeach ?></select></div></div>');
+              $("#form-alunni-typeC").remove();
               break;
 
             case "C":
               $('#tutor').html('<div id="html"> <label class = "form-label" > Tutor: </label> <select class = "form-select" aria - label = "Default select example" name = "id_tutor" required > <option selected disabled > Tutor: </option> <?php foreach ($list_doc as $row) : ?> <option value = "<?php echo $row['CF'] ?>"> <?php echo ($row['nome'] . " " . $row['cognome']) ?> </option> <?php endforeach ?> </select> </div>');
               $("#typeA").remove();
               $("#form-alunni-typeB").remove();
+              $('#alunni-typeC').html('<div id="form-alunni-typeC"><div class="col-md-6 mb-3"><label class="form-label">Alunno:</label><select class="form-select" aria-label="Default select example" name="alunno2" required><option selected disabled>Alunni:</option><?php foreach ($list_al as $row) : ?><option value="<?php echo $row['CF'] ?>"><?php echo ($row['nome'] . " " . $row['cognome']) ?></option><?php endforeach ?></select></div><div class="col-md-6 mb-3"><label class="form-label">Alunno:</label><select class="form-select" aria-label="Default select example" name="alunno3" required><option selected disabled>Alunni:</option><?php foreach ($list_al as $row) : ?><option value="<?php echo $row['CF'] ?>"><?php echo ($row['nome'] . " " . $row['cognome']) ?></option><?php endforeach ?></select></div><div class="col-md-6 mb-3"><label class="form-label">Alunno:</label><select class="form-select" aria-label="Default select example" name="alunno4" required><option selected disabled>Alunni:</option><?php foreach ($list_al as $row) : ?><option value="<?php echo $row['CF'] ?>"><?php echo ($row['nome'] . " " . $row['cognome']) ?></option><?php endforeach ?></select></div><div class="col-md-6 mb-3"><label class="form-label">Alunno:</label><select class="form-select" aria-label="Default select example" name="alunno5" required><option selected disabled>Alunni:</option><?php foreach ($list_al as $row) : ?><option value="<?php echo $row['CF'] ?>"><?php echo ($row['nome'] . " " . $row['cognome']) ?></option><?php endforeach ?></select></div><div class="col-md-6 mb-3"><label class="form-label">Alunno:</label><select class="form-select" aria-label="Default select example" name="alunno6" required><option selected disabled>Alunni:</option><?php foreach ($list_al as $row) : ?><option value="<?php echo $row['CF'] ?>"><?php echo ($row['nome'] . " " . $row['cognome']) ?></option><?php endforeach ?></select></div><div class="col-md-6 mb-3"><label class="form-label">Alunno:</label><select class="form-select" aria-label="Default select example" name="alunno7" required><option selected disabled>Alunni:</option><?php foreach ($list_al as $row) : ?><option value="<?php echo $row['CF'] ?>"><?php echo ($row['nome'] . " " . $row['cognome']) ?></option><?php endforeach ?></select></div><div class="col-md-6 mb-3"><label class="form-label">Alunno:</label><select class="form-select" aria-label="Default select example" name="alunno8" required><option selected disabled>Alunni:</option><?php foreach ($list_al as $row) : ?><option value="<?php echo $row['CF'] ?>"><?php echo ($row['nome'] . " " . $row['cognome']) ?></option><?php endforeach ?></select></div><div class="col-md-6 mb-3"><label class="form-label">Alunno:</label><select class="form-select" aria-label="Default select example" name="alunno9" required><option selected disabled>Alunni:</option><?php foreach ($list_al as $row) : ?><option value="<?php echo $row['CF'] ?>"><?php echo ($row['nome'] . " " . $row['cognome']) ?></option><?php endforeach ?></select></div></div>');
               break;
           }
         });
@@ -252,6 +204,59 @@ session_start();
       }
     });
   </script>
+
+  <?php
+  include_once dirname(__FILE__) . '\..\function\corsi.php';
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    var_dump($_POST);
+    $count = countCorsoByType($_POST['tipologia']);
+    if ($count != -1) {
+      $count = $count + 1;
+      if ($count < 10) {
+        $nome_corso = "Corso_" . $_POST['tipologia'] . "_00" . $count;      //creo il nome del corso unendo la tipologia con il numero del corso
+      } elseif ($count > 9 && $count < 100) {
+        $nome_corso = "Corso_" . $_POST['tipologia'] . "_0" . $count;      //creo il nome del corso unendo la tipologia con il numero del corso
+      } elseif ($count > 99) {
+        $nome_corso = "Corso_" . $_POST['tipologia'] . "_" . $count;      //creo il nome del corso unendo la tipologia con il numero del corso
+      }
+
+      if ($_POST['tipologia'] == 'A' || $_POST['tipologia'] == 'B') {
+        $data = array(
+          "tipologia" => $_POST['tipologia'],
+          "id_quadrimestre" => $_POST['id_quadrimestre'],
+          "id_docente" => $_POST['id_docente'],
+          "id_tutor" => "-1",
+          "materia" => $_POST['materia'],
+          "data_inizio" => $_POST['data_inizio'],
+          "data_fine" => $_POST['data_fine'],
+          "nome_corso" => $nome_corso,
+          "sede" => $_POST['sede'],
+        );
+      }
+      if ($_POST['tipologia'] == 'C') {
+        $data = array(
+          "tipologia" => $_POST['tipologia'],
+          "id_quadrimestre" => $_POST['id_quadrimestre'],
+          "id_docente" => $_POST['id_docente'],
+          "id_tutor" => $_POST['id_tutor'],
+          "materia" => $_POST['materia'],
+          "data_inizio" => $_POST['data_inizio'],
+          "data_fine" => $_POST['data_fine'],
+          "nome_corso" => $nome_corso,
+          "sede" => $_POST['sede'],
+        );
+      }
+      /*$res = addCorso($data);
+      if ($res == 1) {
+      }
+      if ($res == -1) {
+        echo ('<p class="text-danger"><b>Errore!</b></p>');
+      }*/
+    } else {
+      echo ('<p class="text-danger"><b>Errore!</b></p>');
+    }
+  }
+  ?>
 
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
