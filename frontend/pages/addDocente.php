b944da1a495faf1815102efb22db1b85099924ad
Marco-Malanchin <72445677+Marco-Malanchin@users.noreply.github.com>
2023-04-19 12:13:57 +0200

    corretto nome pagina

--- frontend/pages/addDocente.php
+++ frontend/pages/addDocente.php
@@ -8,7 +8,7 @@ session_start();
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Diario | CreaCorso</title>
+  <title>Diario | Aggiungi docente</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
