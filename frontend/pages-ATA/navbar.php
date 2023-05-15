b1e301a3d0b9126fa781476c7b39f655c13f7b8d
Marco-Malanchin <72445677+Marco-Malanchin@users.noreply.github.com>
2023-05-15 17:38:30 +0200

    update pages-ATA

--- frontend/pages-ATA/navbar.php
+++ frontend/pages-ATA/navbar.php
@@ -4,32 +4,40 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   <link rel="stylesheet" href="assets/style.css">
   <link rel="icon" type="image/x-icon" href="../assets/img/logo.png">
 </head>
-<nav class="navbar navbar-expand-lg" style='background-color: #D4020D'>
+<nav class="navbar navbar-expand-lg" style='background-color: #602483'>
   <div class="container-fluid">
-    <a class="navbar-brand fw-bold text-white">Calendario Corsi</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <img src="http://localhost/DiarioProf/frontend/assets/viola-logo.svg" alt="Logo" width="150" height="75">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      <li class="nav-item fw-bold text-white-50">
-          <a class="nav-link" aria-current="page" href="http://localhost/DiarioProf/frontend/pages-ATA/getIncontriToday.php">Incontri di oggi</a>
+        <li class="nav-item fw-bold text-white-50">
+          <a class="nav-link" aria-current="page"
+            href="http://localhost/DiarioProf/frontend/pages-ATA/getIncontriToday.php">Incontri di oggi</a>
         </li>
         <li class="nav-item fw-bold text-white-50">
-          <a class="nav-link" aria-current="page" href="http://localhost/DiarioProf/frontend/pages-ATA/getIncontriTomorrow.php">Incontri di domani</a>
+          <a class="nav-link" aria-current="page"
+            href="http://localhost/DiarioProf/frontend/pages-ATA/getIncontriTomorrow.php">Incontri di domani</a>
         </li>
         <li class="nav-item fw-bold text-white-50">
-          <a class="nav-link" aria-current="page" href="http://localhost/DiarioProf/frontend/pages-ATA/getIncontriNext15Days.php">Incontri dei prossimi 15 giorni</a>
+          <a class="nav-link" aria-current="page"
+            href="http://localhost/DiarioProf/frontend/pages-ATA/getIncontriNext15Days.php">Incontri dei prossimi 15
+            giorni</a>
         </li>
         <li class="nav-item fw-bold text-white-50">
-          <a class="nav-link" aria-current="page" href="http://localhost/DiarioProf/frontend/pages-ATA/countIncontro.php">Visualizza i partecipanti dei corsi dei prossimi 15 giorni</a>
+          <a class="nav-link" aria-current="page"
+            href="http://localhost/DiarioProf/frontend/pages-ATA/countIncontro.php">Visualizza i partecipanti dei corsi
+            dei prossimi 15 giorni</a>
         </li>
       </ul>
     </div>
   </div>
 </nav>
-        
