e37703d940a32d61a6ec1f7d13b5038aff9c3877
michele25-10 <michele.gabri01@gmail.com>
2023-05-17 12:15:23 +0200

    Aggiunti al db user

--- frontend/login.php
+++ frontend/login.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="icon" type="image/x-icon" href="assets/img/logo.svg">
 </head>
 
@@ -17,10 +18,12 @@
             </div>
         </div>
         <h1 class="h3 mb-3 fw-bold">Inserisci le credenziali</h1>
-        <label for="inputEmail" class="sr-only mb-2">Nickname</label>
-        <input type="text" id="inputEmail" class="form-control mb-4" placeholder="nickname" name="nickname" required autofocus>
+        <label for="inputEmail" class="sr-only mb-2">Email</label>
+        <input type="email" id="inputEmail" class="form-control mb-4" placeholder="email" name="email" required
+            autofocus>
         <label for="inputPassword" class="sr-only mb-2">Password</label>
-        <input type="password" id="inputPassword" class="form-control mb-4" placeholder="Password" name="password" required>
+        <input type="password" id="inputPassword" class="form-control mb-4" placeholder="Password" name="password"
+            required>
 
         <?php
         session_start();
@@ -30,14 +33,13 @@
 
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            if (!empty($_POST['nickname']) && !empty($_POST['password'])) { //se la variabile mail o password che devono essere inviate non sono vuote all'ora si invia
+            if (!empty($_POST['email']) && !empty($_POST['password'])) { //se la variabile mail o password che devono essere inviate non sono vuote all'ora si invia
 
                 $pw = hash("sha256", $_POST['password']);
 
                 $data = array(
-                    //Immetto i dati all'interno di data
-                    "nickname" => $_POST['nickname'],
-                    "pw" => $pw,
+                    "email" => $_POST['email'],
+                    "password" => $pw,
                 );
 
                 if (login($data) == -1) {
@@ -55,65 +57,66 @@
         </div>
     </form>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
 </body>
 
 </html>
 
 <style>
-    html,
-    body {
-        height: 100%;
-    }
+html,
+body {
+    height: 100%;
+}
 
-    body {
-        display: -ms-flexbox;
-        display: -webkit-box;
-        display: flex;
-        -ms-flex-align: center;
-        -ms-flex-pack: center;
-        -webkit-box-align: center;
-        align-items: center;
-        -webkit-box-pack: center;
-        justify-content: center;
-        padding-top: 40px;
-        padding-bottom: 40px;
-        background-color: #f5f5f5;
-    }
+body {
+    display: -ms-flexbox;
+    display: -webkit-box;
+    display: flex;
+    -ms-flex-align: center;
+    -ms-flex-pack: center;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    justify-content: center;
+    padding-top: 40px;
+    padding-bottom: 40px;
+    background-color: #f5f5f5;
+}
 
-    .form-signin {
-        width: 100%;
-        max-width: 330px;
-        padding: 15px;
-        margin: 0 auto;
-    }
+.form-signin {
+    width: 100%;
+    max-width: 330px;
+    padding: 15px;
+    margin: 0 auto;
+}
 
-    .form-signin .checkbox {
-        font-weight: 400;
-    }
+.form-signin .checkbox {
+    font-weight: 400;
+}
 
-    .form-signin .form-control {
-        position: relative;
-        box-sizing: border-box;
-        height: auto;
-        padding: 10px;
-        font-size: 16px;
-    }
+.form-signin .form-control {
+    position: relative;
+    box-sizing: border-box;
+    height: auto;
+    padding: 10px;
+    font-size: 16px;
+}
 
-    .form-signin .form-control:focus {
-        z-index: 2;
-    }
+.form-signin .form-control:focus {
+    z-index: 2;
+}
 
-    .form-signin input[type="email"] {
-        margin-bottom: -1px;
-        /* border-bottom-right-radius: 0; */
-        /* border-bottom-left-radius: 0; */
-    }
+.form-signin input[type="email"] {
+    margin-bottom: -1px;
+    /* border-bottom-right-radius: 0; */
+    /* border-bottom-left-radius: 0; */
+}
 
-    .form-signin input[type="password"] {
-        margin-bottom: 10px;
-        /* border-top-left-radius: 0; */
-        /* border-top-right-radius: 0; */
-    }
+.form-signin input[type="password"] {
+    margin-bottom: 10px;
+    /* border-top-left-radius: 0; */
+    /* border-top-right-radius: 0; */
+}
 </style>
