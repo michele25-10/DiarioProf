c86ec9abc4922353623ce702bd6ae3bc51faaaff
michele25-10 <michele.gabri01@gmail.com>
2023-05-29 20:58:28 +0200

    Iniziati i check dei doppi alunni

--- frontend/function/postAlunni.php
+++ frontend/function/postAlunni.php
@@ -7,16 +7,10 @@
     <title>Informazione Corso</title>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css" />
     <script src="https://code.jquery.com/jquery-3.6.3.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 
 <body>
-    <!--<div id="pageloader">
-        <div class="spinner-border text-primary" id="spinner" role="status">
-            <span class="visually-hidden"></span>
-        </div>
-    </div>-->
     <?php
     if (!empty($_POST['submit'])) {
         $stringa = $_POST['submit'];
@@ -25,7 +19,7 @@
         $nome_corso = explode("_", $stringa_esplosa[1]);
         $tipologia = $nome_corso[1];
 
-        var_dump($_POST); 
+        //var_dump($_POST);
 
         include_once dirname(__FILE__) . '\..\function\iscrizione.php';
 
@@ -37,16 +31,74 @@
                 break;
             case 'B':
                 if (!empty($_POST['alunno1']) && !empty($_POST['alunno2']) && !empty($_POST['alunno3']) && !empty($_POST['alunno4']) && !empty($_POST['alunno5']) && !empty($_POST['alunno6'])) {
-                    for ($x = 1; $x < 7; $x++) {
-                        $res = addAlunnoToCorso($id_corso, $_POST["alunno" . $x]);
+
+                    $alunni = array(
+                        0 => $_POST['alunno1'],
+                        1 => $_POST['alunno2'],
+                        2 => $_POST['alunno3'],
+                        3 => $_POST['alunno4'],
+                        4 => $_POST['alunno5'],
+                        5 => $_POST['alunno6'],
+                    );
+
+                    $conteggio = array();
+                    $conteggio = array_count_values($alunni); //conta quante volte è presente nell'array quel valore
+
+                    foreach ($conteggio as $value) {
+                        if ($value > 1) {
+                            echo '<div class="container mt-5"><div class="alert alert-danger" role="alert">Non puoi inserire due volte lo stesso alunno dentro al corso.
+                          </div></div>';
+                            $check = 1;
+                            break;
+                        }
                     }
+
+                    //add 
+                    if ($check != 1) {
+                        for ($x = 1; $x < 7; $x++) {
+                            $res = addAlunnoToCorso($id_corso, $_POST["alunno" . $x]);
+                        }
+                    }
+                } else {
+                    echo '<div class="container mt-5"><div class="alert alert-danger" role="alert">Non sono stati inseriti tutti gli alunni.
+                    </div></div>';
                 }
                 break;
             case 'C':
                 if (!empty($_POST['alunno1']) && !empty($_POST['alunno2']) && !empty($_POST['alunno3']) && !empty($_POST['alunno4']) && !empty($_POST['alunno5']) && !empty($_POST['alunno6']) && !empty($_POST['alunno7']) && !empty($_POST['alunno8']) && !empty($_POST['alunno9'])) {
-                    for ($x = 1; $x < 10; $x++) {
-                        $res = addAlunnoToCorso($id_corso, $_POST["alunno" . $x]);
+
+                    $alunni = array(
+                        0 => $_POST['alunno1'],
+                        1 => $_POST['alunno2'],
+                        2 => $_POST['alunno3'],
+                        3 => $_POST['alunno4'],
+                        4 => $_POST['alunno5'],
+                        5 => $_POST['alunno6'],
+                        6 => $_POST['alunno7'],
+                        7 => $_POST['alunno8'],
+                        8 => $_POST['alunno9'],
+                    );
+                    $conteggio = array();
+                    $conteggio = array_count_values($alunni); //conta quante volte è presente nell'array quel valore
+
+                    foreach ($conteggio as $value) {
+                        if ($value > 1) {
+                            echo '<div class="container mt-5"><div class="alert alert-danger" role="alert">Non puoi inserire due volte lo stesso alunno dentro al corso.
+                          </div></div>';
+                            $check = 1;
+                            break;
+                        }
                     }
+
+                    if ($check != 1) {
+                        for ($x = 1; $x < 10; $x++) {
+                            $res = addAlunnoToCorso($id_corso, $_POST["alunno" . $x]);
+                        }
+                    }
+                } else {
+
+                    echo '<div class="container mt-5"><div class="alert alert-danger" role="alert">Non sono stati inseriti tutti gli alunni.
+                        </div></div>';
                 }
                 break;
         }
@@ -57,21 +109,21 @@
     ?>
 </body>
 <style>
-#pageloader {
-    background: rgba(255, 255, 255, 0.8);
-    height: 100%;
-    position: fixed;
-    width: 100%;
-    z-index: 9999;
-}
+    #pageloader {
+        background: rgba(255, 255, 255, 0.8);
+        height: 100%;
+        position: fixed;
+        width: 100%;
+        z-index: 9999;
+    }
 
-#spinner {
-    left: 50%;
-    margin-left: -32px;
-    margin-top: -32px;
-    position: absolute;
-    top: 50%;
-}
+    #spinner {
+        left: 50%;
+        margin-left: -32px;
+        margin-top: -32px;
+        position: absolute;
+        top: 50%;
+    }
 </style>
 
 </html>
